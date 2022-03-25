84d44ef0e62a819604bb03d27e3c94decdec4479
punica24@njfu.edu.cn <mingyan130824@gmail.com>
2022-03-26 07:04:32 +0800

    A

--- build_personal_website.R
+++ build_personal_website.R
@@ -4,3 +4,7 @@ hugo_version()
 new_site(theme = "ofmla/starter-academic")
 check_gitignore()
 check_content()
+
+## git config --global user.name "NotebookOFXiaoMing"
+## git config --global user.name "punica24@njfu.edu.cn"
+## git add -A
